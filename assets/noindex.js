1109214c558fb9317462967b37838f1fbfd69f43
Said Tantawy <said.tantawy153@gmail.com>
2026-05-09 10:41:59 +0300

    Deployed 2015bd3 with MkDocs version: 1.6.1

---
+++ assets/noindex.js
@@ -0,0 +1,7 @@
+// Prevent search engines from indexing this site
+(function() {
+  var meta = document.createElement('meta');
+  meta.name = 'robots';
+  meta.content = 'noindex, nofollow, noarchive, nosnippet';
+  document.head.appendChild(meta);
+})();
